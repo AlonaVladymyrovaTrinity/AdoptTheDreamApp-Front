2e83203e6b6825912dbfae7509e7e2f065dc6a8f
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-05-16 23:07:46 -0700

    Loader added

---
+++ src/component/layout/Loader/Loader.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+import spinnerSvg from "../../../images/loader.svg";
+import PropTypes from "prop-types";
+
+const SpinnerWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &.small-spinner {
+    transform: scale(0.5);
+  }
+  }
+`;
+//This is functional styled 'Loader' component that displays a spinner with optional size variations
+//based on the given class name.
+const StyledSpinner = ({ className }) => (
+  <SpinnerWrapper className={className}>
+    <img src={spinnerSvg} alt="spinner" />
+  </SpinnerWrapper>
+);
+StyledSpinner.propTypes = {
+  className: PropTypes.string,
+};
+
+export default StyledSpinner;
