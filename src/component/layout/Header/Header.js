ff2bc1ba936e51627003e868bc469f8dcce852ba
Lyubov Rodin <luba@lyubovs-mbp.lan>
2023-05-18 01:29:05 -0400

    code fixed according to reviewer suggestions

--- src/component/layout/Header/Header.js
+++ src/component/layout/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+// import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { TbCat } from "react-icons/tb";
 import { TbDog } from "react-icons/tb";
 import { TbSearch } from "react-icons/tb";
@@ -18,19 +19,30 @@ const Header = () => {
             </div>
           </div>
           <ul className="header_navBar">
-            <li className="navBar_item">Home</li>
-            <li className="navBar_item">Pets</li>
-            <li className="navBar_item">Donate</li>
-            <li className="navBar_item">Contact</li>
-            <li className="navBar_item">About</li>
+            <li className="navBar_item">
+              <Link to="/home">Home</Link>
+            </li>
+            <li className="navBar_item">
+              <Link to="/pets">Pets</Link>
+            </li>
+            <li className="navBar_item">
+              <Link to="/donate">Donate</Link>
+            </li>
+            <li className="navBar_item">
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li className="navBar_item">
+              <Link to="/about">About</Link>
+            </li>
           </ul>
           <div className="header_actions">
-            <div className="header_search">
+            <button className="header_search">
               <TbSearch size="2rem" />
-            </div>
-            {/* <div className="headers_likes">
-              <TbHeart size="2rem" />
-            </div> */}
+            </button>
+            {/* <button className="headers_likes">
+            <TbHeart size="2rem" />
+            </button> */}
+
             <button className="header_logIn">LogIn</button>
           </div>
         </div>
