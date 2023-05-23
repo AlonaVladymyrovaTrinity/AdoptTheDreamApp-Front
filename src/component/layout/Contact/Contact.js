39dba2160e2aae0eaadd971e254a8da2490a8d8b
Anna Ryzhakova <arigakova@gmail.com>
2023-05-23 17:12:06 -0400

    PP-15: initial commit for contact page

--- src/component/layout/Contact/Contact.js
+++ src/component/layout/Contact/Contact.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Contact = () => {
-  return <h1>Contact</h1>;
+    return (
+        <Container className="d-flex align-items-center justify-content-center" style={{ height: '100vh' }}>
+      <Row>
+        <Col className="d-flex justify-content-center">
+        <Card text="black" style={{ width: '20rem' }} className="mb-2">
+            <Card.Header>Contact</Card.Header>
+            <Card.Body>
+                <Card.Title>Center-Left Card</Card.Title>
+                <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                </Card.Text>
+            </Card.Body>
+        </Card>
+        </Col>
+      </Row>
+    </Container>
+    );
 };
 
 export default Contact;
