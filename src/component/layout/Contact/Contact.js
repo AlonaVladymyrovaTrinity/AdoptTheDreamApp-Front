2f68f7a43aba2417c1d6d509af61cff2f3f99a45
Anna Ryzhakova <arigakova@gmail.com>
2023-05-24 17:46:25 -0400

    PP-15: contact page updated

--- src/component/layout/Contact/Contact.js
+++ src/component/layout/Contact/Contact.js
@@ -1,27 +1,40 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import contactPage from '../../../images/contactPage.jpg';
+import './Contact.css';
+//import Container from 'react-bootstrap/Container';
+//import Row from 'react-bootstrap/Row';
+//import Col from 'react-bootstrap/Col';
 
 const Contact = () => {
     return (
-        <Container className="d-flex align-items-center justify-content-center" style={{ height: '100vh' }}>
-      <Row>
-        <Col className="d-flex justify-content-center">
-        <Card text="black" style={{ width: '20rem' }} className="mb-2">
-            <Card.Header>Contact</Card.Header>
-            <Card.Body>
-                <Card.Title>Center-Left Card</Card.Title>
-                <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                </Card.Text>
-            </Card.Body>
-        </Card>
-        </Col>
-      </Row>
-    </Container>
+        <div className="background-container">
+            <div
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    height: '100vh',
+                }}
+            >
+                <div style={{ marginLeft: '10%' }}>
+                    <Card
+                        text="black"
+                        style={{ width: '30rem', height: '20rem' }}
+                        className="mb-2"
+                    >
+                        <Card.Header>Contact Us</Card.Header>
+                        <Card.Body>
+                            <Card.Title>Center-Left Card</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+            </div>
+        </div>
     );
 };
 
