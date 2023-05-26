644643f8c456df8df8cd15f99f3ea85804b17723
Lyubov Rodin <luba@lyubovs-mbp.lan>
2023-05-26 00:01:56 -0400

    no changes

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -3,15 +3,15 @@ import redCat from '../../images/redCat.mp4';
 import './Home.css';
 
 const Home = () => {
-    return (
-        <>
-            <div className="home_banner">
-                <video src={redCat} autoPlay loop muted />
+  return (
+    <>
+      <div className="home_banner">
+        <video src={redCat} autoPlay loop muted />
 
-                <div className="home_banner_content"></div>
-            </div>
-        </>
-    );
+        <div className="home_banner_content"></div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
