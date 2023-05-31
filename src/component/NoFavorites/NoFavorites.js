ad86dd22749d0194254301614f635a47679ff982
Anna Ryzhakova <arigakova@gmail.com>
2023-05-30 21:25:59 -0400

    first commit

--- src/component/NoFavorites/NoFavorites.js
+++ src/component/NoFavorites/NoFavorites.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React from 'react';
+import style from './NoFavorites.module.css';
+import imgNoPets from '../../images/no-animals.png';
+import Button from 'react-bootstrap/Button';
 
 const NoFavorites = () => {
-  return <h1>NoFavorites</h1>;
+  return (
+    <>
+      <div>
+        <h1 className={style['header']}>You don't have favorites pet yet!</h1>
+      </div>
+      <div className={style['no-favorite-page__container']}>
+        <div className={style['no-favorite-wrapper']}>
+          <div>
+            <img
+              className={style['img-page-no-favorite']}
+              src={imgNoPets}
+              alt="no-favorite"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="mb-2">
+        <Button></Button>
+      </div>
+    </>
+  );
 };
 
 export default NoFavorites;
