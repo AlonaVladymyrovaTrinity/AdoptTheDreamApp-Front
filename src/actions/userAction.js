aab50acd0a953ef6c221ce33a716a19879032bcb
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-06-08 23:30:24 -0700

    PP-84 (Part #1): Configured Axios to use credentials and sent a GET request to retrieve user data.

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -21,16 +21,17 @@ export const login = async (
       }
     );
     dispatch({ type: 'LOGIN_SUCCESS' });
-    console.log(res); // Loging the response for testing purposes
+    console.log('login result:' + JSON.stringify(res)); // Loging the response for testing purposes
     setSuccessMessage('User successfully logged in.');
-    // Store token in cookie
-    Cookies.set('token', res.token);
+
+    // Store token and user ID in cookies
+    Cookies.set('token', res.data.token);
+    Cookies.set('userId', res.data.userId);
   } catch (error) {
     dispatch({ type: 'LOGIN_FAILURE' });
     // Error handling: showing an error message
     console.error('Error:', error);
     setErrorMessage('An error occurred during login. Please try again.');
-  } finally {
   }
 };
 
@@ -49,11 +50,13 @@ export const register = async (
       },
     });
     dispatch({ type: 'REGISTER_USER_SUCCESS' });
-    console.log(res); // Loging the response for testing purposes
+    console.log(JSON.stringify(res)); // Loging the response for testing purposes
     console.log(res.statusText); // Loging the statusText response for testing purposes
     setSuccessMessage('User account successfully created. You can login now');
-    // Store token in cookie
-    Cookies.set('token', res.token);
+
+    // Store token and user ID in cookies
+    Cookies.set('token', res.data.token);
+    Cookies.set('userId', res.data.userId);
   } catch (error) {
     dispatch({ type: 'REGISTER_USER_FAIL' });
     // Error handling: showing an error message
@@ -65,6 +68,29 @@ export const register = async (
       );
       console.log(error.response.data.msg);
     }
-  } finally {
+  }
+};
+
+// Load User
+export const loadUser = async (dispatch) => {
+  try {
+    dispatch({ type: 'LOAD_USER_REQUEST' });
+
+    axios.defaults.withCredentials = true;
+    const response = await axios.get(`/api/v1/me/${Cookies.get('userId')}`, {
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${Cookies.get('token')}`,
+      },
+    });
+
+    dispatch({ type: 'LOAD_USER_SUCCESS', payload: response.data.user });
+    console.log('respons: ' + JSON.stringify(response.data.user)); // Loging the response for testing purposes
+  } catch (error) {
+    dispatch({
+      type: 'LOAD_USER_FAIL',
+      payload: error.response?.data?.message || error.message,
+    });
+    console.log('error ' + error);
   }
 };
