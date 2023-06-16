4a2ed52d1f333ea10eb3b6089084869ddb60a606
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-06-16 13:30:11 -0700

    Pictures in cards on Home page and Pets page fixed in accordance with the updated DB scheme and data from database

--- src/actions/petAction.js
+++ src/actions/petAction.js
@@ -1,21 +1,14 @@
 import axios from 'axios';
 
-export const loadPet = async (
-  id,
-  setErrorMessage,
-  dispatch
-) => {
+export const loadPet = async (id, setErrorMessage, dispatch) => {
   dispatch({ type: 'LOAD_PET_REQUEST' });
   try {
-    const response = await axios.get(
-      `/api/v1/pets/${id}`,
-      {
-        withCredentials: true,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    );
+    const response = await axios.get(`/api/v1/pets/${id}`, {
+      withCredentials: true,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
     dispatch({ type: 'LOAD_PET_SUCCESS', payload: response.data.pet });
   } catch (error) {
     dispatch({ type: 'LOAD_PET_FAILURE' });
