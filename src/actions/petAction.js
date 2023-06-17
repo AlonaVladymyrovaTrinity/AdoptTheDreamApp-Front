7e79fa715edace94243d965b6ccd5b618c4b6f09
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-06-16 20:55:23 -0700

    Dropdown lists for animal type and breed implemented

--- src/actions/petAction.js
+++ src/actions/petAction.js
@@ -1,21 +1,15 @@
 import axios from 'axios';
 
-export const loadPet = async (
-  id,
-  setErrorMessage,
-  dispatch
-) => {
+// Get Pet Details
+export const loadPet = async (id, setErrorMessage, dispatch) => {
   dispatch({ type: 'LOAD_PET_REQUEST' });
   try {
-    const response = await axios.get(
-      `/api/v1/pets/${id}`,
-      {
-        withCredentials: true,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    );
+    const response = await axios.get(`/api/v1/pets/${id}`, {
+      withCredentials: true,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
     dispatch({ type: 'LOAD_PET_SUCCESS', payload: response.data.pet });
   } catch (error) {
     dispatch({ type: 'LOAD_PET_FAILURE' });
@@ -40,6 +34,51 @@ export const getPet = async (dispatch) => {
   } catch (error) {
     dispatch({
       type: 'ALL_PET_FAIL',
+      payload: error.response?.data?.message || error.message,
+    });
+  }
+};
+// Get Cat Breeds
+export const getCatBreeds = async (dispatch) => {
+  try {
+    dispatch({ type: 'CAT_BREEDS_REQUEST' });
+    const response = await axios.get(`/api/v1/cat-breeds`, {
+      withCredentials: true,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    //console.log('Get Cat Breeds response: ' + JSON.stringify(response.data)); // logging the response for testing purposes
+    dispatch({
+      type: 'CAT_BREEDS_SUCCESS',
+      payload: response.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: 'CAT_BREEDS_FAIL',
+      payload: error.response?.data?.message || error.message,
+    });
+  }
+};
+
+// Get Dog Breeds
+export const getDogBreeds = async (dispatch) => {
+  try {
+    dispatch({ type: 'DOG_BREEDS_REQUEST' });
+    const response = await axios.get(`/api/v1/dog-breeds`, {
+      withCredentials: true,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    //console.log('Get Dog Breeds response: ' + JSON.stringify(response.data)); // logging the response for testing purposes
+    dispatch({
+      type: 'DOG_BREEDS_SUCCESS',
+      payload: response.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: 'DOG_BREEDS_FAIL',
       payload: error.response?.data?.message || error.message,
     });
   }
--- src/component/Pet/Pets.js
+++ src/component/Pet/Pets.js
@@ -8,14 +8,26 @@ import Row from 'react-bootstrap/Row';
 import style from './Pets.module.css';
 import Nav from 'react-bootstrap/Nav';
 import Form from 'react-bootstrap/Form';
-import { initialState, petsReducer } from '../../reducers/petReducer';
-import { getPet } from '../../actions/petAction';
+import {
+  initialState,
+  petsReducer,
+  catBreedsReducer,
+  dogBreedsReducer,
+} from '../../reducers/petReducer';
+import { getPet, getCatBreeds, getDogBreeds } from '../../actions/petAction';
 import Alert from 'react-bootstrap/Alert';
 
 const Pets = () => {
   const [errorMessage, setErrorMessage] = useState('');
-
   const [state, dispatch] = useReducer(petsReducer, initialState);
+  const [stateCatBreed, dispatchCatBreed] = useReducer(
+    catBreedsReducer,
+    initialState
+  );
+  const [stateDogBreed, dispatchDogBreed] = useReducer(
+    dogBreedsReducer,
+    initialState
+  );
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,6 +42,86 @@ const Pets = () => {
   }, []);
   const pets = useMemo(() => state.pets || {}, [state.pets]);
 
+  const [selectedType, setSelectedType] = useState('');
+  useEffect(() => {
+    const fetchBreeds = async () => {
+      if (selectedType === 'Cat') {
+        try {
+          await getCatBreeds(dispatchCatBreed);
+        } catch (error) {
+          setErrorMessage('');
+          setErrorMessage('Error loading cat breeds');
+        }
+      }
+      if (selectedType === 'Dog') {
+        try {
+          await getDogBreeds(dispatchDogBreed);
+        } catch (error) {
+          setErrorMessage('');
+          setErrorMessage('Error loading dog breeds');
+        }
+      }
+    };
+    fetchBreeds();
+  }, [selectedType]);
+
+  const optionsBreed = {};
+  if (selectedType === 'Cat') {
+    const catBreeds = stateCatBreed.catBreeds || [];
+    catBreeds.forEach((breed) => {
+      optionsBreed[breed] = breed;
+    });
+  }
+  if (selectedType === 'Dog') {
+    const dogBreeds = stateDogBreed.dogBreeds || [];
+    dogBreeds.forEach((breed) => {
+      optionsBreed[breed] = breed;
+    });
+  }
+
+  // const catBreeds = stateCatBreed.catBreeds || [];
+  // const dogBreeds = stateDogBreed.dogBreeds || [];
+
+  // const optionsBreed = {};
+
+  // catBreeds.forEach((breed) => {
+  //   optionsBreed[breed] = breed;
+  // });
+  // dogBreeds.forEach((breed) => {
+  //   optionsBreed[breed] = breed;
+  // });
+
+  const optionsType = {
+    Cat: 'Cat',
+    Dog: 'Dog',
+  };
+  const [selectedBreed, setSelectedBreed] = useState('');
+
+  const handleSelectTypeChange = (event) => {
+    const selectedPetType = event.target.value;
+    setSelectedType(selectedPetType);
+    // localStorage.setItem("selectedPetType", selectedPetType); // Save to local storage
+    //const type = selectedPetType;
+    //alert(type);
+    //API function call here
+    // funcName(
+    //   atribut,
+    //   dispatch
+    // );
+    //setPetsList(petsList);
+  };
+  const handleSelectBreedChange = async (event) => {
+    const selectedPetBreed = event.target.value;
+    setSelectedBreed(selectedPetBreed);
+    const breed = selectedPetBreed;
+    alert(breed);
+    //API function call here
+    // funcName(
+    //   atribut,
+    //   dispatch
+    // );
+    //setPetsList(petsList);
+  };
   return (
     <>
       {errorMessage && (
@@ -47,7 +139,7 @@ const Pets = () => {
           <div className={style.cardsContainerWithSelect}>
             <div className={style.selectBox}>
               <Nav style={{ width: '12rem' }} className="flex-column p-3">
-                <Form.Select
+                {/* <Form.Select
                   className="border-1 bg-transparent rounded mb-3"
                   // className={style['select-option']}
                   aria-label="Default select example"
@@ -55,8 +147,26 @@ const Pets = () => {
                   <option>Type</option>
                   <option value="1">Cat</option>
                   <option value="2">Dog</option>
-                </Form.Select>
+                </Form.Select> */}
                 <Form.Select
+                  id="PetType"
+                  name="PetType"
+                  className="border-1 bg-transparent rounded mb-3"
+                  disabled={!Array.isArray(pets) || pets.length === 0}
+                  onChange={handleSelectTypeChange}
+                  value={selectedType}
+                >
+                  <option disabled value="">
+                    Type
+                  </option>
+                  {Object.entries(optionsType).map(([value, label]) => (
+                    <option key={value} value={value}>
+                      {label}
+                    </option>
+                  ))}
+                </Form.Select>
+
+                {/* <Form.Select
                   className="border-1 bg-transparent rounded mb-3"
                   aria-label="Default select example"
                 >
@@ -64,6 +174,27 @@ const Pets = () => {
                   <option value="1">One</option>
                   <option value="2">Two</option>
                   <option value="3">Three</option>
+                </Form.Select> */}
+                <Form.Select
+                  id="PetBreed"
+                  name="PetBreed"
+                  className={`${style['select-option']} border-1 bg-transparent rounded mb-3`}
+                  disabled={
+                    !Array.isArray(pets) ||
+                    pets.length === 0 ||
+                    selectedType === ''
+                  }
+                  onChange={handleSelectBreedChange}
+                  value={selectedBreed}
+                >
+                  <option disabled value="">
+                    Breed
+                  </option>
+                  {Object.entries(optionsBreed).map(([value, label]) => (
+                    <option key={value} value={value}>
+                      {label}
+                    </option>
+                  ))}
                 </Form.Select>
                 <Form.Select
                   className="border-1 bg-transparent rounded mb-3"
